4bf74418036dd176ad93c14bffe5f8805d249e7f
Deniss Fjodorovs <3starblaze@gmail.com>
2021-05-29 15:48:41 +0300

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    project: "tsconfig.json",
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+};
