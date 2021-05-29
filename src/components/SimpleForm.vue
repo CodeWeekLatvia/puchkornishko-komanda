37cf7f664e4f03074e6812404907fa03142e719a
3starblaze <47479439+3starblaze@users.noreply.github.com>
2021-05-29 18:03:41 +0300

    Add vue (#1)
    * Initialize Vue
    
    * Resolve issues formed by Vue additions
    
    * Scope style and try to add sass
    
    * Create simple form example

---
+++ src/components/SimpleForm.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>{{title}}</h2>
+        <form>
+            <label for="name">Vārds</label>
+            <input type="text" name="name"  />
+            <label for="surname">Uzvārds</label>
+            <input type="text" name="surname"  />
+        </form>
+    </div>
+</template>
+
+<script lang="ts">
+ import Vue from "vue";
+ export default Vue.extend({
+     props: ['title'],
+ });
+</script>
+
+<style scoped="true">
+</style>
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,16 @@
-const message = 'Hello, World!';
-console.log(message);
+import Vue from 'vue';
+import SimpleFormComponent from './components/SimpleForm.vue';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const v = new Vue({
+  el: '#app',
+  template: `
+    <div>
+        <simple-form-component title="Darba devēja forma" />
+        <simple-form-component title="Darba ņēmēja forma" />
+    </div>
+    `,
+  components: {
+    SimpleFormComponent,
+  },
+});
